d1a8f20c93d04022a17160d6ea1583fd3d62fe74
Avyukt Tiwari <526atiwari@frhsd.com>
2025-02-27 17:48:38 -0500

    added javascript

--- script.js
+++ script.js
@@ -19,10 +19,39 @@ quizQuestionArray=['Where was John Adams born?',
 quizQuestionOptionsArray=['A: Massachusetts, B: New York, C: Virginia, D: Pennsylvania',
     'A: John and Abigail, B: John and Susanna, C: John and Martha, D: John and Elizabeth',
 ];
-quizQuestionAnswerArray=['A','B'];
-
+quizQuestionAnswerArray=['A','B','C','D'];
 var quizQuestionNumber = 0;
-document.getElementById('quizQuestion').textContent = quizQuestionArray[quizQuestionNumber];
-document.getElementById('quizQuestionOptions').textContent = quizQuestionOptionsArray[quizQuestionNumber];
-document.getElementById('quizQuestionAnswer').textContent = quizQuestionAnswerArray[quizQuestionNumber];
+document.getElementById('question').textContent = quizQuestionArray[quizQuestionNumber];
+//document.getElementById('quizQuestionOptions').textContent = quizQuestionOptionsArray[quizQuestionNumber];
+//document.getElementById('answer').textContent = quizQuestionAnswerArray[quizQuestionNumber];
 
+var quizanswerchoice2 = quizQuestionOptionsArray[quizQuestionNumber].split(', ');
+document.getElementById('answer-choice1').textContent = quizanswerchoice2[0];
+document.getElementById('answer-choice2').textContent = quizanswerchoice2[1];
+document.getElementById('answer-choice3').textContent = quizanswerchoice2[2];
+document.getElementById('answer-choice4').textContent = quizanswerchoice2[3];
+
+function checkAnswer(){
+    var userAnswer = document.querySelector('input:checked').value;
+    
+    if (userAnswer=== quizQuestionAnswerArray [quizQuestionNumber]){
+        document.getElementById('answer').textContent='Yay, you got it right!';
+    }
+    else{
+       document.getElementById('answer').textContent="Dang it! Try again!";
+    }
+
+
+
+}
+function nextQuestion(){
+quizQuestionNumber++;
+document.getElementById('question').textContent = quizQuestionArray[quizQuestionNumber];
+quizanswerchoice2 = quizQuestionOptionsArray[quizQuestionNumber].split(', ');
+document.getElementById('answer-choice1').textContent = quizanswerchoice2[0];
+document.getElementById('answer-choice2').textContent = quizanswerchoice2[1];
+document.getElementById('answer-choice3').textContent = quizanswerchoice2[2];
+document.getElementById('answer-choice4').textContent = quizanswerchoice2[3];
+document.getElementById('answer').textContent='';
+document.querySelector('input:checked').checked=false;
+}
